87d5b18e59ddeacd3e8794d8f04845d8b7661214
simon-verhoeven <simonaverhoeven@gmail.com>
2021-10-10 20:49:34 +0800

    [feat] feed model

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -18,11 +18,11 @@ export class BaseEntity {
   @Field({ name: 'id' })
   readonly _id: ObjectId;
 
-  @Field()
+  @Field({ name: 'datetime' })
   readonly createdAt: Date;
 
-  @Field()
-  readonly updatedAt: Date;
+  // @Field()
+  // readonly updatedAt: Date;
 
   public equals(compare: any): boolean {
     return this === compare;
--- src/entities/feed.entity.ts
+++ src/entities/feed.entity.ts
@@ -1,16 +1,39 @@
 import { prop as Property, getModelForClass, Ref } from '@typegoose/typegoose';
 import { ObjectId } from 'mongodb';
 
-import { ObjectType, Field } from 'type-graphql';
+import { ObjectType, Field, registerEnumType } from 'type-graphql';
 import { PaginateModel } from 'typegoose-cursor-pagination';
 import { BaseEntity } from './base.entity';
 import { User } from './user.entity';
+
+export enum Verb {
+  SHARE = 'share',
+  LIKE = 'like',
+  POST = 'post',
+  FOLLO = 'follow',
+}
+
+registerEnumType(Verb, {
+  name: 'Verb',
+});
 
 @ObjectType()
 export class Feed extends BaseEntity {
   @Field((type) => ObjectId)
   @Property({ type: ObjectId })
   actor: Ref<User>[];
+
+  @Field((type) => ObjectId)
+  @Property({ type: ObjectId })
+  target: Ref<User>[];
+
+  @Field((type) => Verb)
+  @Property({ type: String, enum: Verb })
+  verb: Verb;
+
+  @Field()
+  @Property()
+  object: string;
 }
 
 export const FeedModel = getModelForClass(Feed) as PaginateModel<Feed, typeof Feed>;
